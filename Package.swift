3cf57f8f6f90bdfe39dcb1463e8301551859c0d4
Simon Støvring <mail@simonbs.dk>
2020-04-25 17:47:24 +0200

    Adds support for Swift Package Manager

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Brotli",
+    platforms: [
+        .iOS(.v9)
+    ],
+    products: [
+        .library(
+            name: "Brotli",
+            targets: ["Brotli"]),
+    ],
+    targets: [
+        .target(
+            name: "Brotli",
+            path: "Brotli",
+            exclude: [
+                "Source/c/common/dictionary.bin",
+                "Source/c/common/dictionary.bin.br",
+            ],
+            sources: ["Classes", "Source/c/common", "Source/c/dec", "Source/c/enc"],
+            publicHeadersPath: "Classes",
+            cSettings: [.headerSearchPath("Source/c/include")])
+    ]
+)
